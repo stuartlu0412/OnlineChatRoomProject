4d2a2b0e2d9bd28180f6be675e57effbbcbeef7c
盧思宇 <stuartlu@lusiyudeMacBook-Pro.local>
2024-11-06 01:14:41 +0800

    first commit

---
+++ code/server.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Server {
+public:
+    Server () // Constructor
+    {
+
+    }
+
+
+private:
+
+};
+
+class Client {
+public:
+    Client () // Client Constructor
+    {
+        
+    }
+
+private:
+
+
+};
+
+int main() {
+
+}
